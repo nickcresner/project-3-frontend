f009f1040b03f569afb35926911ffde44a43ed20
Alex Cwiek <a.i.cwiek@gmail.com>
2017-07-21 17:49:36 +0100

    home page added

--- src/js/config/router.js
+++ src/js/config/router.js
@@ -7,21 +7,25 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider){
   $locationProvider.html5Mode(true);
 
   $stateProvider
-    .state('tripsIndex', {
-      url: '/trips',
-      templateUrl: '/js/views/trips/index.html',
-      controller: 'TripsIndexCtrl as tripsIndex'
-    })
-    .state('tripsShow', {
-      url: '/trips/:id',
-      templateUrl: '/js/views/trips/show.html',
-      controller: 'TripsShowCtrl as tripsShow'
-    })
-    .state('tripsNew', {
-      url: '/trips/new',
-      templateUrl: '/js/views/trips/new.html',
-      controller: 'TripsNewCtrl as tripsNew'
-    });
+  .state('home', {
+    url: '/',
+    templateUrl: '/js/views/static/home.html'
+  })
+  .state('tripsIndex', {
+    url: '/trips',
+    templateUrl: '/js/views/trips/index.html',
+    controller: 'TripsIndexCtrl as tripsIndex'
+  })
+  .state('tripsShow', {
+    url: '/trips/:id',
+    templateUrl: '/js/views/trips/show.html',
+    controller: 'TripsShowCtrl as tripsShow'
+  })
+  .state('tripsNew', {
+    url: '/trips/new',
+    templateUrl: '/js/views/trips/new.html',
+    controller: 'TripsNewCtrl as tripsNew'
+  });
 
   $urlRouterProvider.otherwise('/trips');
 }
