ce9c8a95c1c510472c9788875158f976f2cc4740
Alex Cwiek <a.i.cwiek@gmail.com>
2017-08-03 14:39:45 +0100

    responsive navbar and search field

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('finalProject', ['ngResource', 'ui.router', 'satellizer', 'checklist-model'])
+  .module('finalProject', ['ngResource', 'ui.router', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate'])
   .constant('API_URL', 'http://localhost:3000/api');
 
 console.log('cheese');
